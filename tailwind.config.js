02e6055eabe634a48d7933c05415d738fd5a7cf2
samet <sametcn99@gmail.com>
2024-03-25 01:54:07 +0300

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./src/widgets/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {},
   },
