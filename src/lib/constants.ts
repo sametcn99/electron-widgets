5cb5ad08486d45bc9dce795ae5270593a44b0c37
samet <sametcn99@gmail.com>
2024-03-28 02:26:33 +0300

    create app tray

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,4 @@ export const sourceWidgetsDir = path.join(__dirname, "widgets");
 export const widgetsDir = path.join(homedir(), "Desktop", "widgets");
 export const widgetsJsonPath = path.join(widgetsDir, "widgets.json");
 export const windowsMap = new Map<string, BrowserWindow>();
+export const iconPath = path.join(__dirname, "assets", "electron.png");
--- src/utils/main/app.ts
+++ src/utils/main/app.ts
@@ -1,9 +1,10 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu, nativeImage, screen, Tray } from "electron";
 import { createWindow, createWindowsForWidgets } from "../create-windows";
-import { sourceWidgetsDir, widgetsDir } from "../../lib/constants";
+import { iconPath, sourceWidgetsDir, widgetsDir } from "../../lib/constants";
 import { copyWidgetsDirIfNeeded } from "../utils";
 import { registerMainIPC } from "./ipc";
-
+import is from "electron-is";
+let tray;
 export function runAppFunctions() {
   // Handle creating/removing shortcuts on Windows when installing/uninstalling.
   if (require("electron-squirrel-startup")) {
@@ -17,6 +18,23 @@ export function runAppFunctions() {
     copyWidgetsDirIfNeeded(sourceWidgetsDir, widgetsDir);
     createWindow();
     createWindowsForWidgets();
+
+    const pixelRatio = is.windows()
+      ? screen.getPrimaryDisplay().scaleFactor || 1
+      : 1;
+    const trayIcon = nativeImage.createFromPath(iconPath).resize({
+      width: 16 * pixelRatio,
+      height: 16 * pixelRatio,
+    });
+
+    tray = new Tray(trayIcon);
+    const contextMenu = Menu.buildFromTemplate([
+      { label: "Open", click: () => createWindow() },
+      { label: "Quit", click: () => app.quit() },
+    ]);
+    tray.setToolTip("This is my application.");
+    tray.setContextMenu(contextMenu);
+
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     app.on("activate", () => {
