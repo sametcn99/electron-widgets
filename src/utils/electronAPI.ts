56d6878333adc17cc18be824b66551f0cd3e99d8
samet <sametcn99@gmail.com>
2024-04-18 04:47:46 +0300

    Refactor electronAPI in electronAPI.ts

--- src/utils/electronAPI.ts
+++ src/utils/electronAPI.ts
@@ -5,25 +5,12 @@ import { IpcChannels } from "../lib/channels/ipc-channels";
  * Exposes Electron API to the main world.
  */
 contextBridge.exposeInMainWorld("electronAPI", {
-  /**
-   * Opens an external link in the default browser.
-   * @param url - The URL to open.
-   * @returns A promise that resolves when the operation is complete.
-   */
-  openExternalLink: (url: string) =>
-    ipcRenderer.invoke(IpcChannels.OPEN_EXTERNAL_LINK, url),
-
+  // WIDGET JSON OPERATIONS-----------------------------------------------------
   /**
    * Reads the widgets JSON file.
    * @returns A promise that resolves with the contents of the JSON file.
    */
   readWidgetsJson: () => ipcRenderer.invoke(IpcChannels.READ_WIDGETS_JSON),
-
-  /**
-   * Retrieves the disk usage information.
-   * @returns A promise that resolves with the disk usage information.
-   */
-  getDiskUsage: () => ipcRenderer.invoke(IpcChannels.GET_DISK_USAGE),
 
   /**
    * Writes the widgets JSON file.
@@ -33,6 +20,7 @@ contextBridge.exposeInMainWorld("electronAPI", {
   writeWidgetJson: (data: string) =>
     ipcRenderer.invoke(IpcChannels.WRITE_WIDGETS_JSON, data),
 
+  // WINDOW OPERATIONS---------------------------------------------------------
   /**
    * Creates a new widget window.
    * @param widgetKey - The key of the widget to create.
@@ -49,9 +37,24 @@ contextBridge.exposeInMainWorld("electronAPI", {
   closeWidgetWindow: (widgetKey: string) =>
     ipcRenderer.invoke(IpcChannels.CLOSE_WIDGET_WINDOW, widgetKey),
 
+  // APP OPERATIONS------------------------------------------------------------
   /**
    * Opens a dialog to add a new widget.
    * @returns A promise that resolves when the operation is complete.
    */
   addWidget: () => ipcRenderer.invoke(IpcChannels.ADD_WIDGET_DIALOG),
+
+  /**
+   * Opens an external link in the default browser.
+   * @param url - The URL to open.
+   * @returns A promise that resolves when the operation is complete.
+   */
+  openExternalLink: (url: string) =>
+    ipcRenderer.invoke(IpcChannels.OPEN_EXTERNAL_LINK, url),
+
+  /**
+   * Retrieves the disk usage information.
+   * @returns A promise that resolves with the disk usage information.
+   */
+  getDiskUsage: () => ipcRenderer.invoke(IpcChannels.GET_DISK_USAGE),
 });
