2ddf4b7675a2b894d013ccde079df6f3774189f5
samet <sametcn99@gmail.com>
2024-04-18 04:25:07 +0300

    change visibility buttons to switch button make transparent clock responsive decrease hacker news title size

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,12 +4,14 @@
             class="flex flex-row flex-wrap items-center justify-between w-full p-2 font-bold text-left hover:bg-zinc-900"
             id="widgetsData">
             <div>
-                <h1 class="p-0 text-2xl">{{ widget.title }} </h1>
+                <h1 class="text-xl">{{ widget.title }} </h1>
             </div>
             <div>
-                <button :class="{ 'icon eye-visible': widget.visible, 'icon eye-invisible': !widget.visible, }"
-                    @click="toggleVisibility(widget)">
-                </button>
+                <div class="flex items-center w-12 h-6 duration-300 ease-in-out bg-gray-800 rounded-full"
+                    :class="{ 'bg-gray-600 ': !widget.visible }" @click="toggleVisibility(widget)">
+                    <div class="w-6 h-6 duration-300 ease-in-out transform bg-gray-500 rounded-full shadow-md"
+                        :class="{ 'translate-x-6 bg-green-700': widget.visible, }"></div>
+                </div>
             </div>
         </div>
     </main>
