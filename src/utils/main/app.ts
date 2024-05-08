4f64bddbc123a43a218c0e82f9524439f7c096b6
samet <sametcn99@gmail.com>
2024-05-08 21:34:55 +0300

    chore: Set app to open at login

--- src/utils/main/app.ts
+++ src/utils/main/app.ts
@@ -10,10 +10,12 @@ import { createMainWindow } from "../browser-windows/main-window";
  * Registers the Electron app and sets up necessary event handlers and functionality.
  * This function should be called to initialize the Electron app.
  */
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -29,7 +31,8 @@ app.whenReady().then(() => {
   });
 });
 
+// Set the application to automatically start at login
 app.setLoginItemSettings({
-  openAtLogin: true,
-  enabled: true,
+  openAtLogin: true, // Open the app at login
+  enabled: true, // Enable this setting
 });
