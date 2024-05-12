60a16a99dd3b3236ae9111ceafae79144c51b067
samet <sametcn99@gmail.com>
2024-05-12 19:35:13 +0300

    Remove Footer component

--- src/app/renderer/components/index.ts
+++ src/app/renderer/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Navbar } from "./Navbar.vue";
 export { default as Hero } from "./Hero.vue";
-export { default as Footer } from "./Footer.vue";
 export { default as DropdownButton } from "./DropdownButton.vue";
