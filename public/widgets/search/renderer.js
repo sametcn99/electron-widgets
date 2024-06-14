a403b9b28771c00396a91aa12d24fe219a15a983
samet <sametcn99@gmail.com>
2024-06-14 15:06:55 +0300

    update search w

--- public/widgets/search/renderer.js
+++ public/widgets/search/renderer.js
@@ -3,28 +3,27 @@
 
 // Add event listener to execute code when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Add event listener to search input field for keydown event
-  document
-    .getElementById("searchInput")
-    .addEventListener("keydown", function (event) {
-      if (event.keyCode === 13) {
-        // Check if Enter key is pressed
-        search(); // Call search function
-      }
-    });
+  search(); // Call search function
 });
 
 // Function to perform search
 function search() {
   try {
-    var searchEngine = document.getElementById("searchEngine").value; // Get search engine value
-    var searchTerm = document.getElementById("searchInput").value; // Get search term value
+    document
+      .getElementById("searchInput")
+      .addEventListener("keydown", function (event) {
+        if (event.keyCode === 13) {
+          // Check if Enter key is pressed
+          var searchEngine = document.getElementById("searchEngine").value; // Get search engine value
+          var searchTerm = document.getElementById("searchInput").value; // Get search term value
 
-    if (searchTerm.trim() !== "") {
-      // Check if search term is not empty
-      var searchURL = searchEngine + encodeURIComponent(searchTerm); // Create search URL
-      window.electronAPI.openExternal(searchURL); // Open search URL in external browser
-    }
+          if (searchTerm.trim() !== "") {
+            // Check if search term is not empty
+            var searchURL = searchEngine + encodeURIComponent(searchTerm); // Create search URL
+            window.electronAPI.openExternal(searchURL); // Open search URL in external browser
+          }
+        }
+      });
   } catch (error) {
     console.error("Error occurred during search:", error); // Log any errors that occur during search
   } finally {
